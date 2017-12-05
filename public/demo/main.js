52bf9b15d9bdf76805cca5499597b962f8e57d0b
cleverfox <devel@viruzzz.org>
2017-12-05 11:46:04 +0300

    Make faucet working and web wallet

--- public/demo/main.js
+++ public/demo/main.js
@@ -72177,7 +72177,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var apiEndpoint = 'http://195.3.252.69:43280';
+var apiEndpoint = window.location.protocol+"//"+window.location.hostname+":"+window.location.port;
 
 _axios2.default.defaults.baseURL = apiEndpoint;
 
