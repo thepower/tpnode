76c91b08bcd3b525707f75fff57fab01e4086ac6
Andrew Beyond <spam@lykorian.org.ru>
2018-02-27 14:33:56 +0300

    fix merge artifacts

--- apps/tpnode/src/tpnode_sup.erl
+++ apps/tpnode/src/tpnode_sup.erl
@@ -51,11 +51,10 @@ init([]) ->
             { tpic_sctp, {tpic_sctp, start_link, [TPIC]}, permanent, 5000, worker, []},
             { ledger, {ledger, start_link, []}, permanent, 5000, worker, []},
             { discovery, {discovery, start_link, [#{pid=>discovery, name=>discovery}]}, permanent, 5000, worker, []},
-            { tpnode_announcer, {tpnode_announcer, start_link, [#{}]}, permanent, 5000, worker, []}
             { tpnode_announcer, {tpnode_announcer, start_link, [#{}]}, permanent, 5000, worker, []},
             { crosschain, {crosschain, start_link, [#{}]}, permanent, 5000, worker, []},
             { xchain_dispatcher, {xchain_dispatcher, start_link, []}, permanent, 5000, worker, []},
             xchain_ws_handler:childspec()
-           ]++tpnode_http:childspec()
+           ] ++ tpnode_http:childspec()
          } }.
 
