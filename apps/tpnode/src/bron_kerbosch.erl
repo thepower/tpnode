1b0839b349f1c36cde7bd3ec0d18b0dad5ff26be
law <law12@mail.ru>
2018-03-10 00:14:22 +0300

    В модуль bron_kerbosch добавлены тесты. Плюс and заменены на andalso.

--- apps/tpnode/src/bron_kerbosch.erl
+++ apps/tpnode/src/bron_kerbosch.erl
@@ -1,14 +1,23 @@
 -module(bron_kerbosch).
 -export([max_clique/1]).
 
+-ifndef(TEST).
+-define(TEST,1).
+-endif.
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+
 isConnected(Row, Col, Matrix) ->
-  lists:any(fun(Val) -> {Ind, List} = Val, (Ind =:= Row) and lists:member(Col, List) end, Matrix)
-    and
-  lists:any(fun(Val) -> {Ind, List} = Val, (Ind =:= Col) and lists:member(Row, List) end, Matrix).
+  lists:any(fun(Val) -> {Ind, List} = Val, (Ind =:= Row) andalso lists:member(Col, List) end, Matrix)
+    andalso
+  lists:any(fun(Val) -> {Ind, List} = Val, (Ind =:= Col) andalso lists:member(Row, List) end, Matrix).
 
 check(Candidates, Rejected, Matrix) ->
   lists:foldl(fun(R, Acc1) ->
-    Acc1 and lists:foldl(fun(C, Acc2) ->
+    Acc1 andalso lists:foldl(fun(C, Acc2) ->
       case isConnected(R, C, Matrix) of
         true -> false;
         false -> Acc2
@@ -29,14 +38,14 @@ extend(Candidates, Rejected, Compsub, Results, Matrix) ->
       NewCompsub = [HCandidates|Compsub],
 
       NewCandidates = lists:filter(fun(Val) ->
-        isConnected(Val, HCandidates, Matrix) and (Val =/= HCandidates)
+        isConnected(Val, HCandidates, Matrix) andalso (Val =/= HCandidates)
       end, Candidates),
 
       NewRejected = lists:filter(fun(Val) ->
-        isConnected(Val, HCandidates, Matrix) and (Val =/= HCandidates)
+        isConnected(Val, HCandidates, Matrix) andalso (Val =/= HCandidates)
       end, Rejected),
 
-      NewResults = case (NewCandidates == []) and (NewRejected == []) of
+      NewResults = case (NewCandidates == []) andalso (NewRejected == []) of
                      true -> [lists:sort(NewCompsub)|Results];
                      false -> extend(NewCandidates, NewRejected, NewCompsub, Results, Matrix)
                    end,
@@ -54,3 +63,50 @@ max_clique(Matrix) ->
       false -> Acc
     end
   end, [], Result).
+
+-ifdef(TEST).
+corrupt_matrix_test() ->
+  Matrix = [{}],
+  [
+    ?assertError({badmatch,{}},
+      max_clique(Matrix))
+  ].
+
+empty_matrix_test() ->
+  Matrix = [],
+  [
+    ?assertEqual([],
+      max_clique(Matrix))
+  ].
+
+integer_matrix_test() ->
+  Matrix = [
+    {0, [1, 2]},
+    {1, [0, 2, 3, 4, 5, 6]},
+    {2, [0, 1]},
+    {3, [1, 4, 5, 6]},
+    {4, [1, 3, 5, 6]},
+    {5, [1, 3, 4, 6]},
+    {6, [1, 3, 4, 5]}
+  ],
+  [
+    ?assertEqual([1,3,4,5,6],
+      max_clique(Matrix))
+  ].
+
+binary_matrix_test() ->
+  Matrix = [
+    {<<"a">>, [<<"A">>, <<"B">>]},
+    {<<"A">>, [<<"a">>, <<"B">>, <<"C">>, <<"D">>, <<"E">>, <<"F">>]},
+    {<<"B">>, [<<"a">>, <<"A">>]},
+    {<<"C">>, [<<"A">>, <<"D">>, <<"E">>, <<"F">>]},
+    {<<"D">>, [<<"A">>, <<"C">>, <<"E">>, <<"F">>]},
+    {<<"E">>, [<<"A">>, <<"C">>, <<"D">>, <<"F">>]},
+    {<<"F">>, [<<"A">>, <<"C">>, <<"D">>, <<"E">>]}
+  ],
+  [
+    ?assertEqual([<<"A">>,<<"C">>,<<"D">>,<<"E">>,<<"F">>],
+      max_clique(Matrix))
+  ].
+
+-endif.
