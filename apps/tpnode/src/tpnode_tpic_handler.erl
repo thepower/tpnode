a8acb1298cbca2e17b6abdb8a86b3987c270d317
cleverfox <devel@viruzzz.org>
2018-04-26 13:10:56 +0300

    Add logging for tpic

--- apps/tpnode/src/tpnode_tpic_handler.erl
+++ apps/tpnode/src/tpnode_tpic_handler.erl
@@ -3,70 +3,73 @@
 -export([init/1, handle_tpic/5, routing/1, handle_response/5]).
 
 init(_S) ->
-	lager:info("TPIC Init ~p", [_S]),
-    {ok, #{}}.
+  lager:info("TPIC Init ~p", [_S]),
+  {ok, #{}}.
 
 routing(_State) ->
-    #{ <<"timesync">>=>synchronizer,
-       <<"mkblock">>=>mkblock,
-       <<"blockvote">>=>blockvote,
-       <<"blockchain">>=>blockchain
-     }.
+  #{ <<"timesync">>=>synchronizer,
+     <<"mkblock">>=>mkblock,
+     <<"blockvote">>=>blockvote,
+     <<"blockchain">>=>blockchain
+   }.
 
 handle_tpic(From, _, <<"tping">>, Payload, _State) ->
-    Rnd=rand:uniform(300),
-    timer:sleep(Rnd),
-	lager:info("TPIC tping ~p", [_State]),
-    tpic:cast(tpic, From, <<"delay ", (integer_to_binary(Rnd))/binary,
-                            " pong ", Payload/binary, " from ",
-                            (atom_to_binary(node(), utf8))/binary>>),
-    ok;
+  lager:debug("tping"),
+  Rnd=rand:uniform(300),
+  timer:sleep(Rnd),
+  lager:info("TPIC tping ~p", [_State]),
+  tpic:cast(tpic, From, <<"delay ", (integer_to_binary(Rnd))/binary,
+                          " pong ", Payload/binary, " from ",
+                          (atom_to_binary(node(), utf8))/binary>>),
+  ok;
 
 handle_tpic(From, mkblock, <<"beacon">>, Beacon, _State) ->
-	lager:info("Beacon ~p", [Beacon]),
-	gen_server:cast(topology, {tpic, From, Beacon}),
-	ok;
+  lager:info("Beacon ~p", [Beacon]),
+  gen_server:cast(topology, {tpic, From, Beacon}),
+  ok;
 
 handle_tpic(_From, mkblock, <<>>, Payload, #{authdata:=AD}=_State) ->
-    gen_server:cast(mkblock, {tpic, proplists:get_value(pubkey, AD), Payload}),
-	ok;
+  lager:debug("mkblock from ~p payload ~p",[_From,Payload]),
+  gen_server:cast(mkblock, {tpic, proplists:get_value(pubkey, AD), Payload}),
+  ok;
 
 handle_tpic(_From, service, <<"discovery">>, Payload, _State) ->
-    lager:debug("Service discovery ~p", [Payload]),
-    gen_server:cast(discovery, {got_announce, Payload}),
-    ok;
+  lager:debug("Service discovery from ~p payload ~p", [_From,Payload]),
+  gen_server:cast(discovery, {got_announce, Payload}),
+  ok;
 
 handle_tpic(_From, service, Hdr, Payload, _State) ->
-    lager:info("Service ~p:~p", [Hdr, Payload]),
-    ok;
+  lager:info("Service from ~p hdr ~p payload ~p", [_From, Hdr, Payload]),
+  ok;
 
 handle_tpic(From, _To, <<"kickme">>, Payload, State) ->
-    lager:info("TPIC kick HANDLER from ~p ~p ~p", [From, Payload, State]),
-    close;
+  lager:info("TPIC kick HANDLER from ~p ~p ~p", [From, _To, Payload, State]),
+  close;
 
 handle_tpic(From, blockchain, <<"ping">>, Payload, _State) ->
-    tpic:cast(tpic, From, <<"pong ", Payload/binary, " from ",
-                            (atom_to_binary(node(), utf8))/binary>>),
-    ok;
+  tpic:cast(tpic, From, <<"pong ", Payload/binary, " from ",
+                          (atom_to_binary(node(), utf8))/binary>>),
+  ok;
 
 handle_tpic(From, blockchain, <<"ledger">>, Payload, _State) ->
-    lager:info("Ledger TPIC From ~p p ~p", [From, Payload]),
-    ledger:tpic(From, Payload),
-    ok;
+  lager:info("Ledger TPIC From ~p p ~p", [From, Payload]),
+  ledger:tpic(From, Payload),
+  ok;
 
 handle_tpic(From, To, <<>>, Payload, _State) when To==synchronizer orelse
                                                   To==blockvote orelse
                                                   To==mkblock orelse
                                                   To==blockchain ->
-    gen_server:cast(To, {tpic, From, Payload}),
-    ok;
+  lager:debug("Generic TPIC to ~p from ~p payload ~p", [To,From,Payload]),
+  gen_server:cast(To, {tpic, From, Payload}),
+  ok;
 
 handle_tpic(From, To, Header, Payload, _State) ->
-    lager:info("unknown TPIC ~p from ~p ~p ~p", [To, From, Header, Payload]),
-    ok.
+  lager:info("unknown TPIC ~p from ~p ~p ~p", [To, From, Header, Payload]),
+  ok.
 
 handle_response(From, To, _Header, Payload, State) ->
-    lager:debug("TPIC resp HANDLER from ~p to ~p: ~p ~p ~p", [From, To, _Header, Payload, State]),
-    gen_server:cast(To, {tpic, From, Payload}),
-    ok.
+  lager:debug("TPIC resp HANDLER from ~p to ~p: ~p ~p ~p", [From, To, _Header, Payload, State]),
+  gen_server:cast(To, {tpic, From, Payload}),
+  ok.
 
