0e156e49c903ad49d00ff1a578471230953e5a20
Andrew Beyond <spam@lykorian.org.ru>
2018-06-20 15:47:42 +0300

    compress chain1 alive checker logs into zip file

--- bin/ci_check_chain1.sh
+++ bin/ci_check_chain1.sh
@@ -5,3 +5,7 @@ ct_run -pa _build/test/lib/*/ebin \
       -logdir _build/test/log_chain1 \
       -suite chain1_SUITE \
       -noshell
+
+zip -mr _build/test/log_chain1.zip _build/test/log_chain1/
+mkdir -p _build/test/log_chain1
+mv _build/test/log_chain1.zip _build/test/log_chain1/
