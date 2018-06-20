d2ccc5208d5bb0a7bf1ed71d037b51841e4f00f6
Andrew Beyond <spam@lykorian.org.ru>
2018-06-20 23:53:12 +0300

    run checker using rebar3

--- bin/ci_check_chain1.sh
+++ bin/ci_check_chain1.sh
@@ -1,11 +1,21 @@
 #! /bin/sh
 
 mkdir -p _build/test/log_chain1
-ct_run -pa _build/test/lib/*/ebin \
-      -logdir _build/test/log_chain1 \
-      -suite chain1_SUITE \
-      -noshell
+#ct_run -pa _build/test/lib/*/ebin \
+#      -logdir _build/test/log_chain1 \
+#      -suite chain1_SUITE \
+#      -noshell
 
-zip -mr _build/test/log_chain1.zip _build/test/log_chain1/
+./rebar3 ct --suite=chain1_SUITE \
+         --logdir _build/test/log_chain1 \
+   || exit $?
+
+
+# don't save the logs if everything is OK
+rm -rf _build/test/log_chain1/
 mkdir -p _build/test/log_chain1
-mv _build/test/log_chain1.zip _build/test/log_chain1/
+
+
+
+
+
