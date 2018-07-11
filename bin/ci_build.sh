409f451e8c2ce997f41437f58d135aa26e307ac4
Andrew Beyond <spam@lykorian.org.ru>
2018-07-11 16:14:50 +0300

    unbreak test on vagrant, save ledger bckups to artifacts in case test failed

--- bin/ci_build.sh
+++ bin/ci_build.sh
@@ -2,3 +2,16 @@
 
 ./bin/testnet.sh reset
 make cover
+export rc=$?
+
+# don't save the logs if everything is OK
+if [ $rc -ne 0 ]
+then
+    # save db to artifacts
+    echo "tests failed, saving ledger bckups"
+    tar cfj log/test_ledger_bckups.tar.bz2 /tmp/ledger_bckups/
+fi
+
+rm -rf /tmp/ledger_bckups/
+
+exit $rc
