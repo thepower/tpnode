05b173d45c739366ac8a31c067bb5d00618b9769
Andrew Beyond <spam@lykorian.org.ru>
2018-07-30 13:04:30 +0300

    stop testnet in case of error

--- bin/ci_build.sh
+++ bin/ci_build.sh
@@ -7,6 +7,8 @@ export rc=$?
 # don't save the logs if everything is OK
 if [ $rc -ne 0 ]
 then
+    # stop testnet in case of error
+    ./bin/testnet.sh stop
     # save db to artifacts
     echo "tests failed, saving ledger bckups"
     tar cfj log/test_ledger_bckups.tar.bz2 /tmp/ledger_bckups/
