595ecbbd78d244f1c87e58836bdbfe96c2f2b5bc
Vladimir Goncharov <devel@viruzzz.org>
2018-08-07 18:53:01 +0300

    xchain client dispatcher for new xchain clienr worker

--- apps/tpnode/src/xchain_client.erl
+++ apps/tpnode/src/xchain_client.erl
@@ -19,38 +19,29 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--export([test/0]).
-
-
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
 start_link(Options) ->
   Name = maps:get(name, Options, xchain_client),
-  lager:notice("start ~p", [Name]),
   gen_server:start_link({local, Name}, ?MODULE, Options, []).
-
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
 init(_Args) ->
-  {ok, _} = application:ensure_all_started(gun),
   State = #{
     subs => init_subscribes(#{}),
     chain => blockchain:chain(),
-    cp_to_sub => #{},
-    connect_timer => erlang:send_after(3 * 1000, self(), make_connections),
-    pinger_timer => erlang:send_after(60 * 1000, self(), make_pings)
+    connect_timer => erlang:send_after(3 * 1000, self(), make_connections)
    },
   code:ensure_loaded(xchain_client_handler),
   {ok, State}.
 
 handle_call(state, _From, State) ->
   {reply, State, State};
-
 
 handle_call({add_subscribe, Subscribe}, _From, #{subs:=Subs} = State) ->
   AS=add_sub(Subscribe, Subs),
@@ -62,11 +53,9 @@ handle_call({add_subscribe, Subscribe}, _From, #{subs:=Subs} = State) ->
 handle_call(peers, _From, #{subs:=Subs} = State) ->
   {reply, get_peers(Subs), State};
 
-
 handle_call(_Request, _From, State) ->
   lager:notice("xchain client unknown call ~p", [_Request]),
   {reply, ok, State}.
-
 
 handle_cast(settings, State) ->
   lager:notice("xchain client reload settings"),
@@ -91,158 +80,22 @@ handle_cast(_Msg, State) ->
   lager:error("xchain client unknown cast ~p", [_Msg]),
   {noreply, State}.
 
-handle_info({gun_response, GunPid, GunRef, _IsFin, Code,
-             _Headers}, #{subs:=Subs}=State) ->
-  S1=update_sub(
-       fun(pid, Sub) -> Sub;
-          (ref, Sub) ->
-           case maps:get(request, Sub, undefined) of
-             compat ->
-               if Code==404 ->
-                    gun:ws_upgrade(GunPid, "/xchain/ws"),
-                    Sub#{proto=>0};
-                  true ->
-                    Sub
-               end;
-             _ ->
-               Sub
-           end
-       end, GunPid, GunRef, Subs),
-  {noreply, State#{subs=>S1}};
-
-handle_info({gun_data, GunPid, GunRef, _IsFin,
-             _Payload}, #{subs:=Subs}=State) ->
-  S1=update_sub(
-       fun(pid, Sub) -> Sub;
-          (ref, Sub) ->
-           case maps:get(request, Sub, undefined) of
-             compat ->
-               lager:info("Gun data ~p",[_Payload]),
-               gun:ws_upgrade(GunPid, "/xchain/ws",
-                              [
-                               {<<"sec-websocket-protocol">>, <<"thepower-xchain-v2">>}
-                              ]),
-               maps:without([request, req_ref],
-                            Sub#{proto=>2}
-                           );
-             _ ->
-               Sub
-           end
-       end, GunPid, GunRef, Subs),
-  {noreply, State#{subs=>S1}};
-
-handle_info({ws_request, Peer, Payload}, #{subs:=Subs}=State) ->
-  case maps:find(Peer,Subs) of
-    {ok, #{proto:=P,ws_mode:=true,connection:=Conn}} ->
-      Cmd = xchain:pack(Payload, P),
-      gun:ws_send(Conn, {binary, Cmd}),
-      {noreply, State};
-    {ok, #{}} ->
-      lager:error("Can't make ws request: not connected"),
-      {noreply, State};
-    error ->
-      {noreply, State}
-  end;
-
-handle_info({gun_up, _ConnPid, http}, State) ->
-  lager:notice("xchain client http up"),
-  {noreply, State};
-
-handle_info({gun_ws_upgrade, ConnPid, ok, _Headers}, #{subs:=Subs} = State) ->
-  lager:notice("xchain clientv2 connection upgraded to websocket"),
+handle_info({wrk_down, ConnPid, Reason}, #{subs:=Subs} = State) ->
+  lager:notice("xchain client got close from server for pid ~p: ~p",
+               [ConnPid, Reason]),
   {noreply, State#{
               subs => update_sub(
-                        fun(_,#{proto:=P, channels:=Channels}=Sub) ->
-                            MyNodeId = nodekey:node_id(),
-                            Cmd = xchain:pack({node_id, MyNodeId, maps:keys(Channels)}, P),
-                            gun:ws_send(ConnPid, {binary, Cmd}),
-                            Sub#{ws_mode=>true}
-                        end, ConnPid, undefined, Subs)
-             }
-  };
-
-handle_info({gun_ws, ConnPid, {close, _, _}}, #{subs:=Subs} = State) ->
-  lager:notice("xchain client got close from server for pid ~p", [ConnPid]),
-  {noreply, State#{
-              subs => lost_connection(ConnPid, Subs)
+                        fun(_,Sub) ->
+                            maps:remove(worker,Sub)
+                        end, ConnPid, Subs)
              }};
-
-handle_info({gun_ws, ConnPid, {binary, Bin} }, #{subs:=Subs}=State) ->
-  try
-    {SubKey, #{proto:=P}=Sub}=find_sub(ConnPid, Subs),
-    Handle=case maps:find(ws_handler_pid,Sub) of
-             {ok, Pid} when is_pid(Pid) ->
-               case is_process_alive(Pid) of
-                 true  -> Pid;
-                 false -> default
-               end;
-             error ->
-               default
-           end,
-    if Handle==default ->
-         NewSub = xchain_client_handler:handle_xchain(
-                    xchain:unpack(Bin,P), ConnPid, Sub
-                   ),
-         if NewSub==Sub ->
-              {noreply, State};
-            true ->
-              {noreply, State#{subs=>maps:put(SubKey, NewSub, Subs)}}
-         end
-    end
-  catch
-    Ec:Ee ->
-      S=erlang:get_stacktrace(),
-      lager:error("xchain client msg parse error ~p:~p", [Ec, Ee]),
-      lists:foreach(
-        fun(Se) ->
-            lager:error("at ~p", [Se])
-        end, S),
-      {noreply, State}
-  end;
-
-handle_info({gun_ws, _ConnPid, {text, Msg} }, State) ->
-  lager:error("xchain client got ws text msg: ~p", [Msg]),
-  {noreply, State};
-
-handle_info({gun_down, ConnPid, _, _, _, _}, #{subs:=Subs} = State) ->
-  lager:notice("xchain client lost connection for pid: ~p", [ConnPid]),
-  {noreply, State#{
-              subs => lost_connection(ConnPid, Subs)
-             }};
-
-%%{gun_down, <0.271.0>, http, closed, [], []}
-%%{gun_ws, <0.248.0>, {close, 1000, <<>>}}
-%%{gun_down, <0.248.0>, ws, closed, [], []}
-%%{gun_error, <0.248.0>, {badstate, "Connection needs to be upgraded to Websocket "++
-%%								"before the gun:ws_send/1 function can be used."}}
-%%
-%%{gun_ws_upgrade, <0.248.0>, ok, [{<<"connection">>, <<"Upgrade">>},
-%%{<<"date">>, <<"Sat, 24 Feb 2018 23:42:38 GMT">>},
-%%{<<"sec-websocket-accept">>, <<"vewcPjnW/Rek72GO2D/WPG9/Sz8=">>},
-%%{<<"server">>, <<"Cowboy">>}, {<<"upgrade">>, <<"websocket">>}]}
-%%
-%%{gun_ws, <0.1214.0>, {close, 1000, <<>>}}
 
 handle_info(make_connections, #{connect_timer:=Timer, subs:=Subs} = State) ->
   catch erlang:cancel_timer(Timer),
   NewSubs = make_connections(Subs),
   {noreply, State#{
-              subs => make_subscription(NewSubs),
+              subs => NewSubs,
               connect_timer => erlang:send_after(10 * 1000, self(), make_connections)
-             }};
-
-
-handle_info(make_pings, #{pinger_timer:=Timer, subs:=Subs} = State) ->
-  catch erlang:cancel_timer(Timer),
-  make_pings(Subs),
-  {noreply, State#{
-              pinger_timer => erlang:send_after(30 * 1000, self(), make_pings)
-             }};
-
-
-handle_info({'DOWN', _Ref, process, Pid, _Reason}, #{subs:=Subs} = State) ->
-  {noreply, State#{
-              subs => lost_connection(Pid, Subs)
              }};
 
 handle_info(_Info, State) ->
@@ -255,75 +108,18 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
-
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
-
-make_pings(Subs) ->
-  maps:fold(
-    fun(_Key, #{connection:=ConnPid, ws_mode:=true, proto:=P} = _Sub, Acc) ->
-        Cmd = xchain:pack(ping, P),
-        catch gun:ws_send(ConnPid, {binary, Cmd}),
-        Acc + 1;
-       (_, _, Acc) ->
-        Acc
-    end, 0, Subs).
-
-
-%% --------------------------------------------------------------------
-
-connect_remote(#{address:=Ip, port:=Port} = Sub) ->
-  lager:info("xchain client connecting to ~p ~p", [Ip, Port]),
-  {ok, ConnPid} = gun:open(Ip, Port),
-  monitor(process, ConnPid),
-  case maps:get(protocol, Sub, undefined) of
-    2 ->
-      Sub#{ connection => ConnPid };
-    _ ->
-      Ref=gun:get(ConnPid,"/xchain/api/compat"),
-      Sub#{
-        request => compat,
-        req_ref => Ref,
-        connection => ConnPid
-       }
-  end.
-
-%% --------------------------------------------------------------------
-
-lost_connection(Pid, Subs) ->
-  Cleaner =
-  fun(_Key, #{connection:=Connection, channels:=Channels} = Sub) ->
-      case Connection of
-        Pid ->
-          NewSub = maps:remove(connection, Sub),
-          NewSub1 = maps:remove(ws_mode, NewSub),
-          NewSub2 = maps:remove(node_id, NewSub1),
-
-          % unsubscribe all channels
-          NewSub2#{
-            channels =>
-            maps:map(fun(_Channel, _OldState) -> 0 end, Channels)
-           };
-        _ ->
-          Sub
-      end;
-     (_Key, Sub) ->
-      % skip this subscribe
-      Sub
-  end,
-  maps:map(Cleaner, Subs).
-
-%% --------------------------------------------------------------------
-
 make_connections(Subs) ->
   maps:map(
     fun(_Key, Sub) ->
-        case maps:is_key(connection, Sub) of
+        case maps:is_key(worker, Sub) of
           false ->
             try
               lager:info("xchain client make connection to ~p",[Sub]),
-              connect_remote(Sub)
+              {ok, Pid} = xchain_client_worker:start_link(Sub),
+              Sub#{worker=>Pid}
             catch
               Err:Reason ->
                 lager:info("xchain client got error while connection to remote xchain: ~p ~p",
@@ -332,103 +128,24 @@ make_connections(Subs) ->
             end;
           _ ->
             Sub
-
         end
     end,
     Subs
    ).
 
-%% --------------------------------------------------------------------
-
-subscribe2key(#{address:=Ip, port:=Port}) ->
-  {Ip, Port}.
-
-
-%% --------------------------------------------------------------------
-
-%% #{ {Ip, Port} =>  #{ address =>, port =>, channels =>
-%%						#{ <<"ch1">> => 0, <<"ch2">> => 0, <<"ch3">> => 0}}}
-
-parse_subscribe(#{address:=Ip, port:=Port, channels:=Channels})
-  when is_integer(Port) andalso is_list(Channels) ->
-  NewChannels = lists:foldl(
-                  fun(Chan, ChanStorage) when is_binary(Chan) -> maps:put(Chan, 0, ChanStorage);
-                     (InvalidChanName, _ChanStorage) -> lager:info("xchain client got invalid chan name: ~p", InvalidChanName)
-                  end,
-                  #{},
-                  Channels
-                 ),
-  #{
-                    address => Ip,
-                    port => Port,
-                    channels => NewChannels
-                   };
-
-
-parse_subscribe(Invalid) ->
-  lager:error("xchain client got invalid subscribe: ~p", [Invalid]),
-  throw(invalid_subscribe).
-
-%% --------------------------------------------------------------------
-
-
-check_empty_subscribes(#{channels:=Channels}=_Sub) ->
-  SubCount = maps:size(Channels),
-  if
-    SubCount<1 ->
-      throw(empty_subscribes);
-    true ->
-      ok
-  end.
-
-%% --------------------------------------------------------------------
-
-add_sub(Subscribe, Subs) ->
+add_sub(#{address:=IP,port:=Port}=Subscribe, Subs) ->
   try
-    Parsed = parse_subscribe(Subscribe),
-    Key = subscribe2key(Parsed),
+    Key = {IP,Port},
     NewSub = maps:merge(
-               Parsed,
+               Subscribe,
                maps:get(Key, Subs, #{})
               ),
-    check_empty_subscribes(NewSub),
     maps:put(Key, NewSub, Subs)
   catch
     Reason ->
       lager:error("xchain client can't process subscribe. ~p ~p", [Reason, Subscribe]),
       Subs
   end.
-
-%% --------------------------------------------------------------------
-
-make_subscription(Subs) ->
-
-  Subscriber =
-  fun(_Key, #{connection:=Conn, ws_mode:=true, proto:=P, channels:=Channels}=Sub) ->
-      NewChannels = maps:map(
-                      fun(Channel, 0=_CurrentState) ->
-                          lager:info("xhcain client subscribe to ~p channel", [Channel]),
-                          CCmd = xchain:pack({subscribe, Channel}, P),
-                          Result = gun:ws_send(Conn, {binary, CCmd}),
-                          lager:info("xchain client subscribe result is ~p", [Result]),
-                          1;
-                         (_Channel, CurrentState) ->
-                          % skip this channel
-                          CurrentState
-                      end,
-                      Channels),
-      Sub#{
-        channels => NewChannels
-       };
-     (_, Sub) ->
-      % skip this connection
-      Sub
-  end,
-  maps:map(Subscriber, Subs).
-
-
-%% --------------------------------------------------------------------
-
 
 get_peers(Subs) ->
   Parser =
@@ -440,14 +157,10 @@ get_peers(Subs) ->
   end,
   maps:fold(Parser, #{}, Subs).
 
-
-%% --------------------------------------------------------------------
-
 relay_discovery(_Announce, AnnounceBin, Subs) ->
   Sender =
-  fun(_Key, #{connection:=Conn, ws_mode:=true, proto:=P}, Cnt) ->
-      Cmd = xchain:pack({xdiscovery, AnnounceBin}, P),
-      gun:ws_send(Conn, {binary, Cmd}),
+  fun(_Key, #{worker:=W}, Cnt) ->
+      W ! {send_msg, {xdiscovery, AnnounceBin}},
       Cnt+1;
      (_Key, Sub, Cnt) ->
       lager:debug("Skip relaying to unfinished connection: ~p", [Sub]),
@@ -456,8 +169,6 @@ relay_discovery(_Announce, AnnounceBin, Subs) ->
   Sent = maps:fold(Sender, 0, Subs),
   lager:debug("~p xchain discovery announces were sent", [Sent]),
   ok.
-
-%% --------------------------------------------------------------------
 
 change_settings_handler(#{chain:=Chain, subs:=Subs} = State) ->
   CurrentChain = blockchain:chain(),
@@ -469,8 +180,8 @@ change_settings_handler(#{chain:=Chain, subs:=Subs} = State) ->
 
       % close all active connections
       maps:fold(
-        fun(_Key, #{connection:=ConnPid}=_Sub, Acc) ->
-            catch gun:shutdown(ConnPid),
+        fun(_Key, #{worker:=W}=_Sub, Acc) ->
+            W ! stop,
             Acc+1;
            (_Key, _Sub, Acc) ->
             Acc
@@ -485,72 +196,23 @@ change_settings_handler(#{chain:=Chain, subs:=Subs} = State) ->
        }
   end.
 
-%% -----------------
-
 init_subscribes(Subs) ->
   Config = application:get_env(tpnode, crosschain, #{}),
   ConnectIpsList = maps:get(connect, Config, []),
-  MyChainChannel = xchain:pack_chid(blockchain:chain()),
   lists:foldl(
     fun({Ip, Port}, Acc) when is_integer(Port) ->
         Sub = #{
           address => Ip,
-          port => Port,
-          channels => [MyChainChannel]
+          port => Port
          },
         add_sub(Sub, Acc);
-
        (Invalid, Acc) ->
         lager:error("xhcain client got invalid crosschain connect term: ~p", Invalid),
         Acc
     end, Subs, ConnectIpsList).
 
-
-
-%% -----------------
-
-
-
-%%upgrade_success(ConnPid, Headers) ->
-%%    io:format("Upgraded ~w. Success!~nHeaders:~n~p~n",
-%%        [ConnPid, Headers]),
-%%
-%%    gun:ws_send(ConnPid, {text, "It's raining!"}),
-%%
-%%    receive
-%%        {gun_ws, ConnPid, {text, Msg} } ->
-%%            io:format("got from socket: ~s~n", [Msg])
-%%    end.
-
-test() ->
-  Subscribe = #{
-    address => "127.0.0.1",
-    port => 43312,
-    channels => [<<"test123">>, xchain:pack_chid(2)]
-   },
-  gen_server:call(xchain_client, {add_subscribe, Subscribe}).
-%%    {ok, _} = application:ensure_all_started(gun),
-%%    {ok, ConnPid} = gun:open("127.0.0.1", 43311),
-%%    {ok, _Protocol} = gun:await_up(ConnPid),
-%%
-%%    gun:ws_upgrade(ConnPid, "/"),
-%%
-%%    receive
-%%        {gun_ws_upgrade, ConnPid, ok, Headers} ->
-%%            upgrade_success(ConnPid, Headers);
-%%        {gun_response, ConnPid, _, _, Status, Headers} ->
-%%            exit({ws_upgrade_failed, Status, Headers});
-%%        {gun_error, _ConnPid, _StreamRef, Reason} ->
-%%            exit({ws_upgrade_failed, Reason})
-%%    %% More clauses here as needed.
-%%    after 1000 ->
-%%        exit(timeout)
-%%    end,
-%%
-%%    gun:shutdown(ConnPid).
-
-update_sub(Fun, GunPid, GunRef, Subs) ->
-  case find_sub_by_pid_and_ref(GunPid, GunRef, Subs) of
+update_sub(Fun, GunPid, Subs) ->
+  case find_sub_by_pid_and_ref(GunPid, Subs) of
     undefined ->
       Subs;
     {Matched,Found} ->
@@ -563,21 +225,10 @@ update_sub(Fun, GunPid, GunRef, Subs) ->
       end
   end.
 
-find_sub_by_pid_and_ref(GunPid, GunRef, Subs) ->
+find_sub_by_pid_and_ref(GunPid, Subs) ->
   maps:fold(
-    fun(Key, #{connection:=Pid, req_ref:=RR}, undefined) when
-          Pid==GunPid andalso RR==GunRef ->
-        {ref,Key};
-       (Key, #{connection:=Pid}, undefined) when Pid==GunPid ->
+    fun(Key, #{worker:=Pid}, undefined) when Pid==GunPid ->
         {pid,Key};
-       (_,_, Found) ->
-        Found
-    end, undefined, Subs).
-
-find_sub(GunPid, Subs) ->
-  maps:fold(
-    fun(Key, #{connection:=Pid}=Conn, undefined) when Pid==GunPid ->
-        {Key,Conn};
        (_,_, Found) ->
         Found
     end, undefined, Subs).
--- apps/tpnode/src/xchain_client_worker.erl
+++ apps/tpnode/src/xchain_client_worker.erl
@@ -1,5 +1,5 @@
 -module(xchain_client_worker).
--export([start_link/1,start_link/2,run/2]).
+-export([start_link/1,start_link/2,run/2,ws_mode/3]).
 
 start_link(Sub) ->
   GetFun=fun(node_id) ->
@@ -7,94 +7,120 @@ start_link(Sub) ->
               (chain) ->
                blockchain:chain();
               ({apply_block,Block}) ->
-               txpool:new_tx(Block);
+               txpool:inbound_block(Block);
               ({last,_ChainNo}) ->
                undefined
            end,
-  Pid=spawn(xchain_client_worker,run,[Sub, GetFun]),
+  Pid=spawn(xchain_client_worker,run,[Sub#{parent=>self()}, GetFun]),
   link(Pid),
   {ok, Pid}.
 
 start_link(Sub,test) ->
   GetFun=fun(node_id) ->
-               nodekey:node_id();
-              (chain) -> 5;
-              ({apply_block,#{txs:=TL,header:=H}=_Block}) ->
-               io:format("TXS ~b HDR: ~p~n",[length(TL),maps:get(height,H)]),
-               ok;
-              ({last, 4}) ->
-               <<70,145,201,163,89,206,171,17,192,231,231,17,15,61,24,125,228,102,251,
-                 114,108,13,211,169,147,149,142,17,234,181,98,105>>;
-              ({last,_ChainNo}) ->
-               undefined
-           end,
-  Pid=spawn(xchain_client_worker,run,[Sub, GetFun]),
+             nodekey:node_id();
+            (chain) -> 5;
+            ({apply_block,#{txs:=TL,header:=H}=_Block}) ->
+             io:format("TXS ~b HDR: ~p~n",[length(TL),maps:get(height,H)]),
+             {ok,<<"x">>};
+            ({last, 4}) ->
+             <<70,145,201,163,89,206,171,17,192,231,231,17,15,61,24,125,228,102,251,
+               114,108,13,211,169,147,149,142,17,234,181,98,105>>;
+            ({last,_ChainNo}) ->
+             undefined;
+            (Any) ->
+             io:format("-------~n ERROR ~n~p~n-------~n",[Any]),
+             undefined
+         end,
+  Pid=spawn(xchain_client_worker,run,[Sub#{parent=>self()}, GetFun]),
   link(Pid),
   {ok, Pid}.
 
-run(#{address:=Ip, port:=Port} = Sub, GetFun) ->
+run(#{parent:=Parent, address:=Ip, port:=Port} = Sub, GetFun) ->
+  {ok, _} = application:ensure_all_started(gun),
   process_flag(trap_exit, true),
-  lager:info("xchain client connecting to ~p ~p", [Ip, Port]),
-  {ok, Pid} = gun:open(Ip, Port),
-  receive
-    {gun_up, Pid, http} -> ok
-  after 10000 ->
-          throw('up_timeout')
-  end,
-  Proto=case sync_get_decode(Pid, "/xchain/api/compat.mp") of
-          {200, _, #{<<"ok">>:=true,<<"version">>:=Ver}} -> Ver;
-          {404, _, _} -> 0;
-          _ -> 0
-        end,
-  {ok,UpgradeHdrs}=upgrade(Pid,Proto),
-  lager:debug("Conn upgrade ~p",[UpgradeHdrs]),
-  #{null:=<<"iam">>,
-    <<"chain">>:=HisChain,
-    <<"node_id">>:=_}=reg(Pid, Proto, GetFun),
-  MyLast=GetFun({last, HisChain}),
-  MyChain=GetFun(chain),
-  BlockList=tl(block_list(Pid, Proto, GetFun(chain), last, MyLast, [])),
-  lists:foldl(
+  try
+    lager:info("xchain client connecting to ~p ~p", [Ip, Port]),
+    {ok, Pid} = gun:open(Ip, Port),
+    receive
+      {gun_up, Pid, http} -> ok
+    after 10000 ->
+            throw('up_timeout')
+    end,
+    Proto=case sync_get_decode(Pid, "/xchain/api/compat.mp") of
+            {200, _, #{<<"ok">>:=true,<<"version">>:=Ver}} -> Ver;
+            {404, _, _} -> 0;
+            _ -> 0
+          end,
+    {ok,UpgradeHdrs}=upgrade(Pid,Proto),
+    lager:debug("Conn upgrade ~p",[UpgradeHdrs]),
+    #{null:=<<"iam">>,
+      <<"chain">>:=HisChain,
+      <<"node_id">>:=_}=reg(Pid, Proto, GetFun),
+    MyLast=GetFun({last, HisChain}),
+    MyChain=GetFun(chain),
+    BlockList=tl(block_list(Pid, Proto, GetFun(chain), last, MyLast, [])),
+    lists:foldl(
       fun({_Height, BlockParent},Acc) ->
           #{null:=<<"owblock">>,
             <<"ok">>:=true,
             <<"block">>:=Blk}=pick_block(Pid, Proto, MyChain, BlockParent),
-          Block=block:unpack(Blk),
+          #{txs:=_TL,header:=#{height:=H}}=Block=block:unpack(Blk),
           ok=GetFun({apply_block, Block}),
+          io:format("RB ~p~n",[H]),
           Acc+1
       end, 0, BlockList),
-  [<<"subscribed">>,_]=make_ws_req(Pid, Proto,
-                                   #{null=><<"subscribe">>,
-                                     <<"channel">>=>xchain:pack_chid(MyChain)}
-                                  ),
-  %io:format("SubRes ~p~n",[SubRes]),
-  ok=gun:ws_send(Pid, {binary, xchain:pack(#{null=><<"ping">>}, Proto)}),
-  ws_mode(Pid,Sub#{proto=>Proto},GetFun),
-  gun:close(Pid),
-  done.
+    [<<"subscribed">>,_]=make_ws_req(Pid, Proto,
+                                     #{null=><<"subscribe">>,
+                                       <<"channel">>=>xchain:pack_chid(MyChain)}
+                                    ),
+    %io:format("SubRes ~p~n",[SubRes]),
+    ok=gun:ws_send(Pid, {binary, xchain:pack(#{null=><<"ping">>}, Proto)}),
+    ws_mode(Pid,Sub#{proto=>Proto},GetFun),
+    gun:close(Pid),
+    done
+  catch Ec:Ee ->
+          Parent ! {wrk_down, self(), error},
+          S=erlang:get_stacktrace(),
+          lager:error("xchain client error ~p:~p @ ~p",[Ec,Ee,hd(S)])
+  end.
 
-ws_mode(Pid, #{proto:=Proto}=Sub, GetFun) ->
+ws_mode(Pid, #{parent:=Parent, proto:=Proto}=Sub, GetFun) ->
   receive
     {'EXIT',_,_Reason} ->
       lager:error("Linked process went down. Giving up...."),
+      Cmd = xchain:pack(#{null=><<"goodbye">>, <<"r">>=><<"noparent">>}, Proto),
+      gun:ws_send(Pid, {binary, Cmd}),
       gun:close(Pid),
+      Parent ! {wrk_down, self(), noparent},
       exit;
+    {state, CPid} ->
+      CPid ! {Pid, Sub},
+      ?MODULE:ws_mode(Pid, Sub, GetFun);
     stop ->
+      Cmd = xchain:pack(#{null=><<"goodbye">>, <<"r">>=><<"stop">>}, Proto),
+      gun:ws_send(Pid, {binary, Cmd}),
+      gun:close(Pid),
+      Parent ! {wrk_down, self(), stop},
       done;
+    {send_msg, Payload} ->
+      Cmd = xchain:pack(Payload, Proto),
+      gun:ws_send(Pid, {binary, Cmd}),
+      ?MODULE:ws_mode(Pid, Sub, GetFun);
     {gun_ws, Pid, {binary, Bin}} ->
       Cmd = xchain:unpack(Bin, Proto),
       lager:debug("XChain client got ~p",[Cmd]),
       Sub1=xchain_client_handler:handle_xchain(Cmd, Pid, Sub),
-      ws_mode(Pid, Sub1, GetFun);
+      ?MODULE:ws_mode(Pid, Sub1, GetFun);
     {gun_down,Pid,ws,closed,[],[]} ->
       lager:error("Gun down. Giving up...."),
+      Parent ! {wrk_down, self(), gundown},
       giveup;
     Any ->
       lager:notice("XChain client unknown msg ~p",[Any]),
-      ws_mode(Pid, Sub, GetFun)
+      ?MODULE:ws_mode(Pid, Sub, GetFun)
   after 60000 ->
           ok=gun:ws_send(Pid, {binary, xchain:pack(#{null=><<"ping">>}, Proto)}),
-          ws_mode(Pid, Sub, GetFun)
+          ?MODULE:ws_mode(Pid, Sub, GetFun)
   end.
 
 reg(Pid, Proto, GetFun) ->
