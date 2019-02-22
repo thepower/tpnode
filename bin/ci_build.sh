ba71cc578fd7b0051bf80819583886d0f687c98a
Andrew Beyond <spam@lykorian.org.ru>
2019-02-22 12:47:02 +0300

    clean blog files on ci

--- bin/ci_build.sh
+++ bin/ci_build.sh
@@ -29,6 +29,7 @@ fi
 # save logs
 tar cfj log/test_logs.tar.bz2 ./log/*.log ./log/*.blog
 find ./log -name '*.log' -delete
+find ./log -name '*.blog' -delete
 
 # cleanup
 rm -rf /tmp/ledger_bckups/
