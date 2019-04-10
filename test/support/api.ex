1764eef4e0c17af206b4576e93e837f521d42f2b
Andrew Beyond <spam@lykorian.org.ru>
2019-04-10 09:53:10 +0300

    small refactoring

--- test/support/api.ex
+++ test/support/api.ex
@@ -1,35 +1,33 @@
 defmodule TPHelpers.API do
-  alias TPHelpers.Resolver
+
+  @resolver TPHelpers.Resolver
 
   # get current transaction status
-  def api_get_tx_status(tx_id), do: api_get_tx_status(tx_id, Resolver.get_base_url())
-  def api_get_tx_status(tx_id, base_url), do: :tpapi.get_tx_status(tx_id, base_url)
+  def api_get_tx_status(tx_id, base_url \\ get_base_url()), do: :tpapi.get_tx_status(tx_id, base_url)
 
   # post encoded and signed transaction using API
-  def api_post_transaction(transaction) do
-    api_post_transaction(transaction, Resolver.get_base_url())
-  end
-
-  def api_post_transaction(transaction, base_url) do
+  def api_post_transaction(transaction, base_url \\ get_base_url()) do
     :tpapi.commit_transaction(transaction, base_url)
   end
 
-  # get current seq for wallet
-  def api_get_wallet_seq(wallet) do
-    api_get_wallet_seq(wallet, Resolver.get_base_url())
+  # get the current seq for wallet
+  def api_get_wallet_seq(wallet, base_url \\ get_base_url()) do
+    :tpapi.get_wallet_seq(wallet, base_url)
   end
 
-  def api_get_wallet_seq(wallet, base_url), do: :tpapi.get_wallet_seq(wallet, base_url)
-
-  # get the blockchain height
-  def api_get_height() do
-    api_get_height(Resolver.get_base_url())
-  end
-
-  def api_get_height(base_url), do: :tpapi.get_height(base_url)
+  # get the current blockchain height
+  def api_get_height(base_url \\ get_base_url()), do: :tpapi.get_height(base_url)
 
   # get block headers for the hash
   def api_get_block_info(hash, opts \\ []) do
-    :tpapi.get_blockinfo(hash, Resolver.get_base_url(Keyword.get(opts, :node, nil)))
+    :tpapi.get_blockinfo(hash, get_base_url(get_node(opts)))
+  end
+
+  defp get_base_url(node \\ nil) do
+    @resolver.get_base_url(node)
+  end
+
+  defp get_node(opts) do
+    Keyword.get(opts, :node, nil)
   end
 end
