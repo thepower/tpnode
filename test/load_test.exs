c95245b90268a2b0708150f71c01b7ef02e9c3e8
Andrew Beyond <spam@lykorian.org.ru>
2019-04-11 16:21:25 +0300

    implement workers

--- test/load_test.exs
+++ test/load_test.exs
@@ -12,7 +12,7 @@ ExUnit.start
 
 {:ok, files} = File.ls("./test/support")
 
-Enum.each files, fn(file) ->
+Enum.each files, fn (file) ->
   Code.require_file "support/#{file}", __DIR__
 end
 
@@ -20,29 +20,33 @@ end
 defmodule LoadTest do
   use ExUnit.Case, async: false
 
+  alias TPHelpers.TxSender
+
   import TPHelpers
-  import TPHelpers.API
+  import TPHelpers.TxGen
 
   # define nodes where send transaction to
   @nodes ["c4n1", "c4n2", "c4n3"]
 
   # how many transactions should each worker send
-  @txs_per_worker 5
+  @txs_per_worker 1000
 
-  @endless_currency "SK"
+  @tx_currency "SK"
 
 
   def setup_all do
     for x <- @nodes, do: start_node(x)
 
-    status = @nodes |> wait_nodes
+    status = @nodes
+             |> wait_nodes
     logger "nodes status: ~p", [status]
 
     assert :ok == status
   end
 
   def clear_all do
-    for x <- @nodes, do: stop_node(x)
+#    for x <- @nodes, do: stop_node(x)
+    :ok
   end
 
   #  @tag :skip
@@ -50,33 +54,169 @@ defmodule LoadTest do
   test "load test" do
     setup_all()
 
-    # create wallets
-    wallets =
+    # create endless wallets
+    senders =
+      register_wallets(endless: @tx_currency, nodes: @nodes, priv_key: get_wallet_priv_key())
+    logger("senders: ~p", [senders])
+
+    # create destinations
+    dests = register_wallets(nodes: @nodes, priv_key: get_wallet_priv_key())
+    logger("destinations: ~p", [dests])
+
+    # generate transactions for each worker
+    txs =
+      generate_txs(
+        senders,
+        dests,
+        nodes: @nodes,
+        priv_key: get_wallet_priv_key(),
+        txs_per_node: @txs_per_worker
+      )
+
+    logger("txs: ~p", [txs])
+
+    {:ok, sup_pid} = DynamicSupervisor.start_link(
+      [
+        name: :wrk_keeper,
+        strategy: :one_for_one
+      ]
+    )
+    IO.puts("supervisor pid: #{inspect sup_pid}")
+
+    wrk_pids =
       for node <- @nodes do
-        logger("register new wallet, node = ~p", [node])
+        wrk_name = "wrk_#{node}"
+        IO.puts("starting worker #{wrk_name}")
 
-        status = register_wallet(get_wallet_priv_key(), node: node)
+        wrk_opts = [
+          name: wrk_name,
+          node: node,
+          txs: Map.get(txs, node)
+        ]
 
-        logger("wallet registration status: ~p", [status])
-        assert match?(%{"ok" => true}, status)
-
-        wallet_address = Map.get(status, "res", nil)
-        refute wallet_address == nil
-
-        logger("new wallet has been registered: ~p", [wallet_address])
-
-        # make endless
-        status = make_endless(wallet_address, @endless_currency, node: node)
-        assert match?(%{"ok" => true, "res" => "ok"}, status)
-
-        wallet_address
+        {:ok, pid} = DynamicSupervisor.start_child(
+          :wrk_keeper,
+          %{
+            restart: :temporary,
+            id: wrk_name,
+            type: :worker,
+            start: {TxSender, :start_link, [wrk_opts]}
+          }
+        )
+        IO.puts("worker pid: #{inspect pid}")
+        pid
       end
 
-    logger("wallets: ~p", [wallets])
+    logger("all worker pids: ~p", [wrk_pids])
+
+    for wrk_pid <- wrk_pids do
+      GenServer.cast(wrk_pid, :start)
+    end
+
+    wait_workers(wrk_pids)
+
+    logger("clean up")
+
+    res = DynamicSupervisor.stop(:wrk_keeper)
+    IO.puts("stop the supervisor: #{inspect res}")
+
+    clear_all()
   end
 
+  def wait_workers(wrk_pids) do
+    workers_state =
+      wrk_pids
+      |> Enum.map(fn (wrk_pid) ->
+        {:ok, %{mode: mode, sent: sent}} = GenServer.call(wrk_pid, :get_progress)
+        if mode == :working do
+          {wrk_pid, sent}
+        end
+      end)
+      |> Enum.filter(&(&1))
+
+    logger("workers state: ~p", [workers_state])
+
+    if length(workers_state) > 0 do
+      :timer.sleep(1000)
+      wait_workers(wrk_pids)
+    end
+  end
+
+
+  @spec generate_txs(list(), list(), keyword()) :: map()
+  def generate_txs(senders, dests, opts \\ []) do
+    nodes = Keyword.get(opts, :nodes, @nodes)
+    priv_key = Keyword.get(opts, :priv_key, get_wallet_priv_key())
+    tx_count = Keyword.get(opts, :txs_per_node, 10)
+    currency = Keyword.get(opts, :currency, @tx_currency)
+
+    {wallets_map, [], []} =
+      Enum.reduce(
+        nodes,
+        {%{}, senders, dests},
+        fn (node, {acc, [sender | tail_senders], [dest | tail_dests]}) ->
+          {Map.put(acc, node, %{s: sender, d: dest}), tail_senders, tail_dests}
+        end
+      )
+
+    nodes
+    |> Enum.reduce(
+         %{},
+         fn (node, acc) ->
+           %{s: src, d: dst} = Map.get(wallets_map, node)
+
+           txs =
+             for tx_no <- 1..tx_count do
+               message = "tx #{tx_no}"
+               amount = tx_no
+               :tx.pack(
+                 construct_and_sign_tx(
+                   src,
+                   dst,
+                   currency,
+                   amount,
+                   message: message,
+                   priv_key: priv_key
+                 )
+               )
+             end
+           Map.put(acc, node, txs)
+         end
+       )
+  end
+
+  @spec get_wallet_priv_key() :: binary()
   def get_wallet_priv_key() do
     :address.parsekey("5KHwT1rGjWiNzoZeFuDT85tZ6KTTZThd4xPfaKWRUKNqvGQQtqK")
+  end
+
+  @spec register_wallets(keyword()) :: list(binary())
+  def register_wallets(opts \\ []) do
+    endless = Keyword.get(opts, :endless, false)
+    nodes = Keyword.get(opts, :nodes, @nodes)
+    priv_key = Keyword.get(opts, :priv_key, get_wallet_priv_key())
+
+    for node <- nodes do
+      logger("register new wallet, node = ~p", [node])
+
+      status = register_wallet(priv_key, node: node)
+
+      logger("wallet registration status: ~p", [status])
+      assert match?(%{"ok" => true}, status)
+
+      wallet_address = Map.get(status, "res", nil)
+      refute wallet_address == nil
+
+      logger("new wallet has been registered: ~p", [wallet_address])
+
+      # make endless
+      if is_binary(endless) do
+        status = make_endless(wallet_address, endless, node: node)
+        assert match?(%{"ok" => true, "res" => "ok"}, status)
+      end
+
+      wallet_address
+    end
   end
 
 end
---
+++ test/support/tx_sender.ex
@@ -0,0 +1,128 @@
+defmodule TPHelpers.TxSender do
+  @moduledoc """
+    Implements worker, which send transactions via the API.
+  """
+
+  use GenServer
+
+  import TPHelpers.API
+
+
+  def start_link(opts \\ []) do
+    #    IO.puts("start link #{inspect opts}")
+    GenServer.start_link(__MODULE__, opts)
+  end
+
+  def init(opts \\ []) do
+    name = Keyword.get(opts, :name, :unknown)
+    IO.puts("init worker #{name}")
+
+    Process.flag(:trap_exit, true)
+
+    {
+      :ok,
+      %{
+        counter: 0,
+        node: Keyword.get(opts, :node, nil),
+        txs: Keyword.get(opts, :txs, []),
+        name: name,
+        mode: :idle,
+        tx_ids: []
+      }
+    }
+  end
+
+  def handle_call(:get_count, _from, %{counter: count} = state) do
+    {:reply, {:ok, count}, state}
+  end
+
+  def handle_call(:get_mode, _from, %{mode: mode} = state) do
+    {:reply, {:ok, mode}, state}
+  end
+
+  def handle_call(:get_progress, _from, %{mode: mode, counter: sent} = state) do
+    {:reply, {:ok, %{mode: mode, sent: sent}}, state}
+  end
+
+  def handle_call(:state, _from, state) do
+    IO.puts("state request")
+    {:reply, {:ok, state}, state}
+  end
+
+
+  def handle_call(unknown, _from, state) do
+    IO.puts("got unknown call: #{inspect unknown}")
+    {:reply, :ok, state}
+  end
+
+  def handle_cast(:start, %{mode: mode} = state) when mode == :idle do
+    IO.puts("start sending transactions")
+    GenServer.cast(self(), :send)
+    {:noreply, %{state | mode: :working}}
+  end
+
+  def handle_cast(:stop, %{mode: mode} = state) when mode == :working do
+    IO.puts("stop sending transactions (requested)")
+    {:noreply, %{state | mode: :idle}}
+  end
+
+  # go to :idle mode if no more transactions left
+  def handle_cast(:send, %{mode: mode, txs: []} = state) when mode == :working do
+    IO.puts("stop sending transactions (all txs were sent)")
+    {:noreply, %{state | mode: :idle}}
+  end
+
+  # send one transaction
+  def handle_cast(
+        :send,
+        %{mode: mode, txs: [tx | tail], node: node, tx_ids: tx_ids, counter: count} = state
+      )
+      when mode == :working do
+
+    tx_id = send_tx(tx, node)
+    GenServer.cast(self(), :send)
+    {:noreply, %{state | txs: tail, tx_ids: [tx_id | tx_ids], counter: count + 1}}
+  end
+
+  def handle_cast(unknown, state) do
+    IO.puts("got unknown cast: #{inspect unknown}")
+    {:noreply, state}
+  end
+
+
+  # handle the trapped exit call
+  def handle_info({:EXIT, _from, reason}, state) do
+    IO.puts "exiting"
+
+    {:stop, reason, state}
+  end
+
+  def handle_info(:stop_worker, %{name: name} = _state)do
+    IO.puts("worker: #{name} going to stop")
+    exit(:normal)
+  end
+
+  def handle_info(unknown, state) do
+    IO.puts("got unknown info: #{inspect unknown}")
+    {:noreply, state}
+  end
+
+
+  def terminate(reason, %{name: name}) do
+    IO.puts "terminate: #{name}, reason: #{reason}}"
+    :normal
+  end
+
+  defp send_tx(tx, node) do
+    try do
+      res = api_post_transaction(tx, node: node)
+      tx_id = Map.get("txid", res, nil)
+      tx_id
+    catch
+      ec, ee ->
+        :utils.print_error("tx send failed", ec, ee, :erlang.get_stacktrace())
+        nil
+    end
+  end
+
+end
