a0b255d2d7d77a5ec03dd2d788f6c98e67bb8e0b
Andrew Beyond <spam@lykorian.org.ru>
2019-04-26 16:18:03 +0300

    update chain1 ips

--- bin/ci_check_chain1.sh
+++ bin/ci_check_chain1.sh
@@ -17,7 +17,7 @@ then
     rm -rf _build/test/log_chain1/
     mkdir -p _build/test/log_chain1
 else
-        for ip in "2001:bc8:4700:2500::40d" "2001:bc8:4700:2500::305" "2001:bc8:4400:2700::1263" "2001:bc8:4400:2700::1265" "2001:bc8:4400:2700::2341"
+        for ip in "2001:bc8:4700:2500::131f" "2001:bc8:4700:2500::121b" "2001:bc8:4400:2700::1263" "2001:bc8:4400:2700::1265" "2001:bc8:4400:2700::2341"
         do
             (
             ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip "cd thepower; . /opt/erl/activate;  ./bin/thepower eval 'blockchain ! runsync.'"
