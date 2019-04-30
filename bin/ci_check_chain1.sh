f41c542cc6970ec0262f437c9055c723bd4e2c33
Andrew Beyond <spam@lykorian.org.ru>
2019-04-30 13:39:17 +0300

    increase wait time to 10 seconds, remove runsync crutch

--- bin/ci_check_chain1.sh
+++ bin/ci_check_chain1.sh
@@ -17,12 +17,13 @@ then
     rm -rf _build/test/log_chain1/
     mkdir -p _build/test/log_chain1
 else
-        for ip in "2001:bc8:4700:2500::131f" "2001:bc8:4700:2500::121b" "2001:bc8:4400:2700::1263" "2001:bc8:4400:2700::1265" "2001:bc8:4400:2700::2341"
-        do
-            (
-            ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip "cd thepower; . /opt/erl/activate;  ./bin/thepower eval 'blockchain ! runsync.'"
-             ) &
-        done
+    echo "test failed"
+#        for ip in "2001:bc8:4700:2500::131f" "2001:bc8:4700:2500::121b" "2001:bc8:4400:2700::1263" "2001:bc8:4400:2700::1265" "2001:bc8:4400:2700::2341"
+#        do
+#            (
+#            ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$ip "cd thepower; . /opt/erl/activate;  ./bin/thepower eval 'blockchain ! runsync.'"
+#             ) &
+#        done
 fi
 
 exit $rc
