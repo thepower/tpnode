8e641f75c41261d7ae20a40fd952cba0f358e3e1
cleverfox <devel@viruzzz.org>
2021-04-05 17:53:59 +0300

    pass mean time and entropy to smartcontract VM

--- apps/tpnode/src/contract_wasm.erl
+++ apps/tpnode/src/contract_wasm.erl
@@ -13,8 +13,8 @@ handle_tx(Tx, Ledger, GasLimit, GetFun) ->
   Entropy=GetFun(entropy),
   MeanTime=GetFun(mean_time),
   Settings=GetFun(settings),
-  XtraFields=#{ mean_time => MeanTime,
-                entropy => Entropy },
+  XtraFields=#{ "mean_time" => MeanTime,
+                "entropy" => Entropy },
   vm:run(fun(VMPid) ->
              VMPid ! {run, 
                       tx:pack(Tx),
