ab24c4e561e85d76edecf1ddbbc3bbdf4f3be051
Vladimir <devel@viruzzz.org>
2024-08-25 17:48:24 +0000

    new mledger with state merkle tree in db

--- apps/tpnode/src/mledger.erl
+++ apps/tpnode/src/mledger.erl
@@ -1,10 +1,11 @@
 -module(mledger).
+% vim: tabstop=2 shiftwidth=2 expandtab
 -include("include/tplog.hrl").
 -compile({no_auto_import,[get/1]}).
 -export([start_db/0,deploy4test/2, put/2,get/1,get_vers/1,hash/1,hashl/1]).
 -export([bi_create/6,bi_set_ver/2]).
 -export([bals2patch/1, apply_patch/2]).
--export([dbmtfun/3]).
+-export([dbmtfun/4]).
 -export([mb2item/1]).
 -export([get_lstore/2,get_lstore_map/2]).
 -export([getfun/1]).
@@ -14,8 +15,11 @@
 -export([addr_proof/1, bi_decode/1]).
 -export([rollback/2]).
 -export([apply_backup/1]).
+-export([tables/0]).
+-export([account_to_mt/2, account_mt_check/2]).
 
 -record(bal_items, { address,version,key,path,introduced,value }).
+-record(address_storage, { address,key,value }).
 -opaque bal_items() :: #bal_items{}.
 -export_type([bal_items/0]).
 
@@ -30,7 +34,12 @@
         }).
 
 tables() ->
-  [table_descr(bal_items),table_descr(ledger_tree)].
+  [
+   table_descr(bal_items),
+   table_descr(ledger_tree),
+   table_descr(address_storage)
+  ].
+
 
 table_descr(bal_items) ->
   {
@@ -50,11 +59,20 @@ table_descr(ledger_tree) ->
    fun() ->
        case rockstable:get(mledger,undefined,{ledger_tree,<<"R">>,'_'}) of
          not_found ->
-           rockstable:put(mledger,undefined,{ledger_tree,<<"R">>,db_merkle_trees:empty()});
+           rockstable:put(mledger,undefined,{ledger_tree,<<"R">>,db_merkle_trees2:empty()});
          [{ledger_tree,<<"R">>,_}] ->
            ok
        end
    end
+  };
+
+table_descr(address_storage) ->
+  {
+   address_storage,
+   record_info(fields, address_storage),
+   [address, key],
+   [],
+   undefined
   }.
 
 rm_rf(Dir) ->
@@ -81,7 +99,6 @@ deploy4test(LedgerData, TestFun) ->
 
 start_db() ->
   Path=utils:dbpath(mledger),
-  %Path="db/mledger_" ++ atom_to_list(node()) ++ ".db",
   case rockstable:open_db(mledger,Path,tables()) of
     ok -> ok;
     {error,alias_in_use} -> ok
@@ -242,16 +259,16 @@ get_raw(Address, notrans) ->
 get(Address, notrans) ->
   Fields=[amount,lastblk,pubkey,seq,t,vm,ublk,code],
   Raw=lists:flatten(
-    lists:foldl(
-      fun(Key,Acc) ->
-          [rockstable:get(mledger, env, #bal_items{
-                                           address=Address,
-                                           version=latest,
-                                           key=Key,
-                                           path=[],
-                                           _ ='_'
-                                          })|Acc]
-      end,[],Fields)),
+        lists:foldl(
+          fun(Key,Acc) ->
+              [rockstable:get(mledger, env, #bal_items{
+                                               address=Address,
+                                               version=latest,
+                                               key=Key,
+                                               path=[],
+                                               _ ='_'
+                                              })|Acc]
+          end,[],Fields)),
   if Raw == [] ->
        undefined;
      true ->
@@ -272,9 +289,9 @@ get(Address, notrans) ->
 
 get(Address, trans) ->
   {atomic,List}=rockstable:transaction(mledger,
-                  fun()->
-                      get(Address, notrans)
-                  end),
+                                       fun()->
+                                           get(Address, notrans)
+                                       end),
   List.
 
 hashl(BIs) when is_list(BIs) ->
@@ -354,79 +371,165 @@ bals2patch([{A,Bal}|Rest], PRes) ->
   Res=maps:fold(FoldFun, PRes, Bal),
   bals2patch(Rest,Res).
 
-dbmtfun(get, Key, Acc) ->
-  [{ledger_tree, Key, Val}] = rockstable:get(mledger,env,{ledger_tree,Key}),
+dbmtfun(get, Key, Acc, {DbName,ledger_tree}) ->
+  [{ledger_tree, Key, Val}] = rockstable:get(DbName,env,{ledger_tree,Key}),
   {Val,Acc};
 
-dbmtfun(put, {Key, Value}, Acc) ->
+dbmtfun(put, {Key, Value}, Acc, {DbName,ledger_tree}) ->
 %  io:format("Put ~p ~p~n",[Key,Value]),
-  ok=rockstable:put(mledger,env,{ledger_tree,Key,Value}),
+  ok=rockstable:put(DbName,env,{ledger_tree,Key,Value}),
   Acc;
 
-dbmtfun(del, Key, Acc) ->
-  ok=rockstable:del(mledger,env,{ledger_tree,Key}),
+dbmtfun(del, Key, Acc, {DbName, ledger_tree}) ->
+  ok=rockstable:del(DbName,env,{ledger_tree,Key}),
+  Acc;
+
+dbmtfun(get, Key, Acc, {DbName, address_storage, Addr}) ->
+  [{address_storage,Addr,Key,Val}] = rockstable:get(DbName,env,{address_storage,Addr,Key,'_'}),
+%  io:format("get ~p key ~p~n - val ~p~n",[Addr, Key, Val]),
+  {Val,Acc};
+
+dbmtfun(put, {Key, Value}, Acc, {DbName, address_storage, Addr}) ->
+%  io:format("put ~p key ~p~n - val ~p~n",[Addr,Key,Value]),
+  ok=rockstable:put(DbName,env,{address_storage, Addr, Key, Value}),
+  Acc;
+
+dbmtfun(del, Key, Acc, {DbName, address_storage, Addr}) ->
+%  io:format("del ~p acc ~p~n",[{address_storage, Addr, Key},Acc]),
+  ok=rockstable:del(DbName,env,{address_storage, Addr, Key}),
   Acc.
 
-do_apply([], _) ->
+account_mt(_Table, #bal_items{key=ublk},Acc) -> Acc;
+account_mt(Table, #bal_items{address=Addr, key=K, path=P, value= <<>>}=_BI,Acc) ->
+  %io:format("Del ~p~n",[_BI]),
+  db_merkle_trees2:delete(
+    sext:encode({K,P}),
+    {fun dbmtfun/4, Acc, {Table, address_storage, Addr} }
+   );
+account_mt(Table, #bal_items{address=Addr, key=K, path=P, value=V}=_BI,Acc) ->
+  %io:format("Put ~p~n",[_BI]),
+  db_merkle_trees2:enter(
+    sext:encode({K,P}),
+    sext:encode(V),
+    {fun dbmtfun/4, Acc, {Table, address_storage, Addr} }
+   ).
+
+account_mt_check(Table, Address) ->
+  BIs=rockstable:get(Table, undefined, #bal_items{
+                    address=Address,
+                    version=latest,
+                    _ ='_'
+                   }),
+  H1=hashl(BIs),
+  Fun=fun() ->
+        db_merkle_trees2:root_hash({fun mledger:dbmtfun/4, #{},
+                      {Table,
+                       address_storage,Address}
+                       }) end,
+  case rockstable:transaction(Table, Fun) of
+    {atomic,H2} when H1==H2 ->
+      {ok, H1};
+    {atomic,H2} ->
+      {error, {H1, H2}};
+    Other ->
+      {error, {H1, Other}}
+  end.
+
+
+account_to_mt(Table, Address) ->
+  case rockstable:get(Table,env,{address_storage, Address, <<"R">>,'_'}) of
+    not_found ->
+      rockstable:put(Table, env, {address_storage, Address, <<"R">>,db_merkle_trees2:empty()}),
+      BIs=rockstable:get(Table, env, #bal_items{
+                        address=Address,
+                        version=latest,
+                        _ ='_'
+                       }),
+      lists:foldl(fun (BI,A) ->
+                account_mt(Table, BI, A)
+            end, undefined, BIs),
+      db_merkle_trees2:balance({fun dbmtfun/4,
+                    #{},
+                    {Table, address_storage, Address}
+                   });
+    [{address_storage,Address,<<"R">>,_}] ->
+      exists
+  end.
+
+
+do_apply(Table, [], _) ->
   {ok,
-   db_merkle_trees:root_hash({fun dbmtfun/3, #{} })
+   db_merkle_trees2:root_hash({fun dbmtfun/4, #{}, {Table, ledger_tree} })
   };
 
-do_apply([E1|_]=Patches, HeiHash) when is_record(E1, bal_items) ->
+do_apply(Table, [E1|_]=Patches, HeiHash) when is_record(E1, bal_items) ->
   ChAddrs=lists:usort([ Address || #bal_items{address=Address} <- Patches ]),
 
-  WriteWHeight=fun(#bal_items{value=NewVal}=BalItem,Height) ->
-                   case rockstable:get(mledger, env, BalItem) of
-                     not_found ->
-                       rockstable:put(mledger, env,BalItem#bal_items{introduced=Height});
-                     [#bal_items{introduced=_OVer, value=OVal}=OldBal]  ->
-                       if(OVal == NewVal) ->
-                           ok;
-                         NewVal == <<>> ->
-                           rockstable:put(mledger, env, bi_set_ver(OldBal,Height)),
-                           rockstable:del(mledger, env, OldBal);
-                         true ->
-                           rockstable:put(mledger, env, bi_set_ver(OldBal,Height)),
-                           rockstable:put(mledger, env, BalItem#bal_items{introduced=Height})
-                       end
-                   end
-               end,
+  %ensure merkle tree for exists for all changed accounts
+  lists:foreach(
+    fun(Address) ->
+        account_to_mt(Table, Address)
+    end,
+    ChAddrs
+   ),
+
   case HeiHash of
     _ when is_integer(HeiHash) ->
       lists:foreach( fun(E) ->
-                         WriteWHeight(E,HeiHash)
+                         write_with_height(Table,E,HeiHash)
                      end, Patches);
     {Height, Hash} when is_integer(Height), is_binary(Hash) ->
       lists:foreach( fun(E) ->
-                         WriteWHeight(E, Height)
+                         write_with_height(Table,E, Height)
                      end, Patches),
       lists:foreach(fun(Address) ->
-                        WriteWHeight(
-                          bi_create(Address,latest,ublk,[],Height,Hash),
-                          Height
-                         )
-                        end,ChAddrs);
+                        write_with_height(Table,
+                                          bi_create(Address,latest,ublk,[],Height,Hash),
+                                          Height
+                                         )
+                    end,ChAddrs);
     _ ->
-      lists:foreach( fun (Patch) ->
-                         rockstable:put(mledger, env, Patch)
+      lists:foreach( fun (BalItem) ->
+                         rockstable:put(Table, env, BalItem),
+                         account_mt(Table, BalItem, undefined)
                      end, Patches)
   end,
   NewMT=apply_mt(ChAddrs),
-  RH=db_merkle_trees:root_hash({fun dbmtfun/3,
-                                NewMT
-                               }),
-  %io:format("RH ~p~n",[RH]),
+  RH=db_merkle_trees2:root_hash({fun dbmtfun/4,
+                   NewMT,
+                   {Table, ledger_tree}
+                  }),
   {ok, RH};
 
-do_apply([E1|_],_) ->
+
+do_apply(_Table, [E1|_],_) ->
   throw({bad_patch, E1}).
 
+write_with_height(Table,#bal_items{value=NewVal}=BalItem,Height) ->
+  case rockstable:get(Table, env, BalItem) of
+    not_found ->
+      rockstable:put(Table, env,BalItem#bal_items{introduced=Height}),
+      account_mt(Table, BalItem, undefined);
+    [#bal_items{introduced=_OVer, value=OVal}=OldBal]  ->
+      if(OVal == NewVal) ->
+          ok;
+        NewVal == <<>> ->
+          rockstable:put(Table, env, bi_set_ver(OldBal,Height)),
+          rockstable:del(Table, env, OldBal),
+          account_mt(Table, BalItem, undefined);
+        true ->
+          rockstable:put(Table, env, bi_set_ver(OldBal,Height)),
+          rockstable:put(Table, env, BalItem#bal_items{introduced=Height}),
+          account_mt(Table, BalItem, undefined)
+      end
+  end.
+
 apply_backup(Patches) ->
-  do_apply(Patches, undefined).
+  do_apply(mledger, Patches, undefined).
 
 apply_patch(Patches, check) ->
   F=fun() ->
-        NewHash=do_apply(Patches, undefined),
+        NewHash=do_apply(mledger, Patches, undefined),
         throw({'abort',NewHash})
     end,
   {aborted,{throw,{abort,NewHash}}}=rockstable:transaction(mledger,F),
@@ -435,7 +538,7 @@ apply_patch(Patches, check) ->
 
 apply_patch(Patches, {commit, HeiHash, ExpectedHash}) ->
   F=fun() ->
-        {ok,LH}=do_apply(Patches, HeiHash),
+        {ok,LH}=do_apply(mledger, Patches, HeiHash),
         case LH == ExpectedHash of
           true ->
             ?LOG_DEBUG("check and commit expected ~p actual ~p",
@@ -459,10 +562,15 @@ apply_patch(Patches, {commit, HeiHash}) ->
   %?LOG_INFO("Ledger dump ~s",[Filename]),
   %wrfile(Filename, Patches),
   %dump_ledger("pre",Filename),
-  F=fun() ->
-          do_apply(Patches, HeiHash)
-    end,
-  {atomic,Res}=rockstable:transaction(mledger, F),
+  {atomic,Res}=rockstable:transaction(mledger,
+                                      fun() ->
+                                          try
+                                            do_apply(mledger, Patches, HeiHash)
+                                          catch Ec:Ee:S ->
+                                                  throw({Ec,Ee,S})
+                                          end
+                                      end
+                                     ),
   %dump_ledger("post",Filename),
   %io:format("Apply hash ~p~n",[Res]),
   Res.
@@ -473,51 +581,60 @@ apply_patch(Patches, {commit, HeiHash}) ->
 %                 ).
 
 
+rollback_internal(Table, Height) ->
+  ToDelete=rockstable:get(Table,env,#bal_items{introduced=Height,_='_'}),
+  ToRestore=rockstable:get(Table,env,#bal_items{version=Height,_='_'}),
+
+  OldAddr=lists:usort([ Address || #bal_items{address=Address} <- ToRestore ]),
+  NewAddr=lists:usort([ Address || #bal_items{address=Address} <- ToDelete ]),
+  Addr2Del=NewAddr -- OldAddr,
+
+  lists:foreach(
+    fun(#bal_items{version=latest}=BalItem) ->
+        rockstable:del(Table,env,BalItem),
+        account_mt(Table, BalItem#bal_items{value= <<>>},undefined)
+    end,
+    ToDelete),
+
+  lists:foreach(
+    fun(#bal_items{}=BalItem) ->
+        rockstable:del(Table,env,BalItem),
+        OldBal1=bi_set_ver(BalItem,latest),
+        rockstable:put(Table,env,OldBal1),
+        account_mt(Table, OldBal1,undefined)
+    end,
+    ToRestore),
+
+  del_mt(Addr2Del),
+  NewMT=apply_mt(OldAddr),
+  db_merkle_trees2:root_hash({fun dbmtfun/4,
+                                 NewMT,
+                                 {mledger, ledger_tree}
+                                }).
+
+  
 rollback(Height, ExpectedHash) ->
-	case application:get_env(tpnode,rollback_snapshot,false) of
-		true ->
-			Backup=utils:dbpath("rollback_"++integer_to_list(Height)++"_"++binary_to_list(hex:encodex(ExpectedHash))++"_bck"),
-			?LOG_NOTICE("Backup before rollback ~s",[Backup]),
-			rockstable:backup(mledger,Backup);
-		false ->
-			ok
-	end,
-	case
-  rockstable:transaction(mledger,
-    fun() ->
-        ToDelete=rockstable:get(mledger,env,#bal_items{introduced=Height,_='_'}),
-        ToRestore=rockstable:get(mledger,env,#bal_items{version=Height,_='_'}),
+  rollback(mledger, Height, ExpectedHash).
 
-        OldAddr=lists:usort([ Address || #bal_items{address=Address} <- ToRestore ]),
-        NewAddr=lists:usort([ Address || #bal_items{address=Address} <- ToDelete ]),
-        Addr2Del=NewAddr -- OldAddr,
-
-        lists:foreach(
-          fun(#bal_items{version=latest}=BalItem) ->
-              rockstable:del(mledger,env,BalItem)
-          end,
-          ToDelete),
-
-        lists:foreach(
-          fun(#bal_items{}=BalItem) ->
-			  rockstable:del(mledger,env,BalItem),
-              OldBal1=bi_set_ver(BalItem,latest),
-              rockstable:put(mledger,env,OldBal1)
-          end,
-          ToRestore),
-
-        del_mt(Addr2Del),
-        NewMT=apply_mt(OldAddr),
-        RH=db_merkle_trees:root_hash({fun dbmtfun/3,
-                                      NewMT
-                                     }),
-
-        if(RH==ExpectedHash) ->
-            {ok, RH};
-          true ->
-            throw({hash_mismatch, RH, ExpectedHash})
-        end
-    end) of
+rollback(Table, Height, ExpectedHash) ->
+  case application:get_env(tpnode,rollback_snapshot,false) of
+    true ->
+      Backup=utils:dbpath("rollback_"++atom_to_list(Table)++"_"++integer_to_list(Height)++"_"++binary_to_list(hex:encodex(ExpectedHash))++"_bck"),
+      ?LOG_NOTICE("Backup ~p before rollback ~s",[Table, Backup]),
+      rockstable:backup(Table,Backup);
+    false ->
+      ok
+  end,
+  Trans=fun() ->
+            RH=rollback_internal(Table, Height),
+            if(RH==ExpectedHash) ->
+                {ok, RH};
+              true ->
+                throw({hash_mismatch, RH, ExpectedHash})
+            end
+        end,
+  case
+    rockstable:transaction(Table, Trans) of
     {atomic, {ok, Hash}} -> {ok, Hash};
     {aborted, Err} -> {error,Err}
   end.
@@ -525,23 +642,39 @@ rollback(Height, ExpectedHash) ->
 del_mt(ChAddrs) ->
   lists:foldl(
     fun(Addr,Acc) ->
-        db_merkle_trees:delete(Addr, {fun dbmtfun/3,Acc})
+      db_merkle_trees2:delete(Addr, {fun dbmtfun/4,
+                       Acc,
+                       {mledger, ledger_tree}
+                      })
     end, #{}, ChAddrs).
 
 apply_mt(ChAddrs) ->
-  NewLedger=[ {Address, hash(Address)} || Address <- ChAddrs ],
-  ?LOG_DEBUG("Apply ledger ~p",[NewLedger]),
   lists:foldl(
-    fun({Addr,Hash},Acc) ->
-        db_merkle_trees:enter(Addr, Hash, {fun dbmtfun/3,Acc})
-    end, #{}, NewLedger),
-  db_merkle_trees:balance({fun dbmtfun/3,#{}}).
+    fun(Addr,Acc) ->
+      db_merkle_trees2:balance({fun dbmtfun/4,
+                    #{},
+                    {mledger, address_storage, Addr}
+                   }),
+      Hash=db_merkle_trees2:root_hash(
+           {fun mledger:dbmtfun/4,
+          #{},
+          {mledger, address_storage, Addr}
+           }),
+      db_merkle_trees2:enter(Addr, Hash, {fun dbmtfun/4,
+                        Acc,
+                        {mledger, ledger_tree}
+                         })
+  end, #{}, ChAddrs),
+  db_merkle_trees2:balance({fun dbmtfun/4,
+              #{},
+              {mledger, ledger_tree}
+               }).
 
 addr_proof(Address) ->
   {atomic,Res}=rockstable:transaction(mledger,
     fun() -> {
-         db_merkle_trees:root_hash({fun dbmtfun/3, #{}}),
-        db_merkle_trees:merkle_proof(Address,{fun dbmtfun/3, #{}})
+    db_merkle_trees2:root_hash({fun dbmtfun/4, #{}, {mledger, ledger_tree}}),
+    db_merkle_trees2:merkle_proof(Address,{fun dbmtfun/4, #{}, {mledger, ledger_tree}})
         }
     end),
   Res.
--- test/mledger_test.erl
+++ test/mledger_test.erl
@@ -3,49 +3,82 @@
 
 mledger_test() ->
   Addr= <<128,1,64,0,2,0,0,1>>,
+  D0 = #{amount => #{
+					 <<"FTT">> => 110,
+					 <<"SK">> => 10,
+					 <<"TST">> => 26
+					},
+		 seq => 1,
+		 state => #{ <<0>> => <<0>>, <<1>> => <<1>>, <<2>> => <<1>>,
+					 <<"updated">> => <<"old_value_to_update">>,
+					 <<"deleted">> => <<"old_value_to_delete">>
+				   },
+		 t => 1512047425350,
+		 lastblk => <<0:64>>,
+		 lstore=><<129,196,6,101,120,105,115,116,115,1>>
+		},
+  D1 = #{amount => #{
+					 <<"FTT">> => 100,
+					 <<"SK">> => 10,
+					 <<"TST">> => 26
+					},
+		 state => #{ <<0>> => <<1>>, <<1>> => <<>>, <<3>> => <<4>>,
+					 <<"created">> => <<"new_item">>,
+					 <<"updated">> => <<"updated_value">>,
+					 <<"deleted">> => <<>>
+				   },
+		 lstore=><<129,196,6,101,120,105,115,116,115,0>>,
+		 seq => 555,
+		 changes => [state, seq]
+		},
   Test=fun(_) ->
-           Bals=#{Addr =>
-                  #{amount => #{<<"FTT">> => 100,<<"SK">> => 10,<<"TST">> => 26},
-                    state => #{ <<0>> => <<1>>, <<1>> => <<>> },
-                    lstore=><<129,196,6,101,120,105,115,116,115,0>>,
-                    seq => 555,
-                    changes => [state, seq]
-                   }
-                 },
-
-           L0=mledger:get(Addr),
+			   T0=[
+				   ?assertMatch({ok,<<"old_value_to_delete">>},mledger:get_kpv(Addr,state,<<"deleted">>)),
+				   ?assertMatch({ok,<<"old_value_to_update">>},mledger:get_kpv(Addr,state,<<"updated">>)),
+				   ?assertMatch({ok,#{ <<"FTT">> := 110, <<"SK">> := 10, <<"TST">> := 26 }},mledger:get_kpv(Addr,amount,[])),
+				   ?assertMatch(undefined,mledger:get_kpv(Addr,state,<<"created">>)),
+				   ?assertMatch({ok,<<0>>},mledger:get_kpv(Addr,state,<<0>>)),
+				   ?assertMatch({ok,<<1>>},mledger:get_kpv(Addr,state,<<1>>))
+				  ],
 
            {ok, LedgerHash0}=mledger:apply_patch([],check),
-           io:format("LH0 ~p~n", [LedgerHash0]),
-           Patch=mledger:bals2patch(maps:to_list(Bals)),
-           {ok, LedgerHash}=mledger:apply_patch(Patch,{commit,2}),
-           io:format("LH1 ~p~n", [LedgerHash]),
+
+           Patch=mledger:bals2patch(maps:to_list(#{Addr => D1})),
+		   %io:format("Patch ~p~n",[Patch]),
+           {ok, LedgerHash1}=mledger:apply_patch(Patch,{commit,2}),
            {ok, LedgerHash2}=mledger:apply_patch([],check),
-           io:format("LH2 ~p~n", [LedgerHash2]),
-           L1=mledger:get(Addr),
 
-           io:format("~p~n",[Bals]),
+           %io:format("LH0 ~p~n", [LedgerHash0]),
+           %io:format("LH1 ~p~n", [LedgerHash1]),
+           %io:format("LH2 ~p~n", [LedgerHash2]),
 
-           io:format("0 ~p~n", [L0]),
-           io:format("1 ~p~n", [L1]),
-           %io:format("L ~p~n", [mledger:get_vers(Addr)]),
-           [
-            ?assertEqual([], [])
-           ]
+		   T1=[
+			   ?assertMatch({ok,<<1>>},mledger:get_kpv(Addr,state,<<0>>)),
+			   ?assertMatch(undefined,mledger:get_kpv(Addr,state,<<1>>)),
+			   ?assertMatch(undefined,mledger:get_kpv(Addr,state,<<"deleted">>)),
+			   ?assertMatch({ok,<<"updated_value">>},mledger:get_kpv(Addr,state,<<"updated">>)),
+			   ?assertMatch({ok,<<"new_item">>},mledger:get_kpv(Addr,state,<<"created">>)),
+			   ?assertMatch({ok,#{ <<"FTT">> := 100, <<"SK">> := 10, <<"TST">> := 26 }},mledger:get_kpv(Addr,amount,[])),
+			   ?assertNotEqual(LedgerHash0, LedgerHash1),
+			   ?assertEqual(LedgerHash1, LedgerHash2)
+			  ],
+		   {ok,LedgerHash3}=mledger:rollback(2,LedgerHash0),
+
+		   %io:format("~p~n",[ mledger:get_vers(Addr) ]),
+
+		   T3=[
+			   ?assertMatch({ok,<<"old_value_to_delete">>},mledger:get_kpv(Addr,state,<<"deleted">>)),
+			   ?assertMatch({ok,<<"old_value_to_update">>},mledger:get_kpv(Addr,state,<<"updated">>)),
+			   ?assertMatch(undefined,mledger:get_kpv(Addr,state,<<"created">>)),
+			   ?assertMatch({ok,#{ <<"FTT">> := 110, <<"SK">> := 10, <<"TST">> := 26 }},mledger:get_kpv(Addr,amount,[])),
+			   ?assertEqual(LedgerHash0, LedgerHash3)
+			  ],
+		   T0++T1++T3
        end,
   Ledger=[
           {Addr,
-           #{amount => #{
-               <<"FTT">> => 110,
-               <<"SK">> => 10,
-               <<"TST">> => 26
-              },
-             seq => 1,
-             state => #{ <<0>> => <<0>>, <<1>> => <<1>>, <<2>> => <<1>> },
-             t => 1512047425350,
-             lastblk => <<0:64>>,
-             lstore=><<129,196,6,101,120,105,115,116,115,1>>
-            }
+		   D0
+           
           }
          ],
   mledger:deploy4test(Ledger, Test).
